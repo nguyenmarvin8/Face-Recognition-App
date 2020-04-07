b352f62ef4e53f82ab6bb0063780a956a8af67f1
nguyenmarvin8 <nguyenmarvin8@gmail.com>
2020-04-06 23:56:57 -0700

    make it better

--- facerecognition/src/components/Signin/Signin.js
+++ facerecognition/src/components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://pacific-harbor-94272.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
